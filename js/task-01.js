43aee31483a4dcc742c1dfebf240174b920b3d74
OLEH FEDORCHUK <fedorchuk2018@gmail.com>
2023-05-10 23:09:52 +0100

    update

---
+++ js/task-01.js
@@ -0,0 +1,11 @@
+const ulCategoriesEl = document.querySelector("#categories");
+console.log(ulCategoriesEl);
+
+const countCategories = ulCategoriesEl.querySelectorAll("h2");
+// console.log(countCategories);
+console.log(`Number of categories: ${countCategories.length}`);
+
+console.log(ulCategoriesEl.nextSibling);
+// const ulRef = document.querySelectorAll("h2");
+// console.log(`Number of categories: ${ulRef.length}`);
+// console.log(window);
