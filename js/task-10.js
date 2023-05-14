ee95c012d172277953407a182af81213996f4be3
OLEH FEDORCHUK <fedorchuk2018@gmail.com>
2023-05-14 11:42:58 +0100

    update

--- js/task-10.js
+++ js/task-10.js
@@ -3,3 +3,35 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const divControlsEl = document.querySelector("#controls");
+const inputEl = document.querySelector("#controls input");
+const buttonCreateEl = document.querySelector("#controls button[data-create]");
+const divBoxesEl = document.querySelector("#boxes");
+const buttonDestroyEl = document.querySelector(
+  "#controls button[data-destroy]"
+);
+
+buttonCreateEl.addEventListener("click", () => {
+  createBoxes(inputEl.value);
+});
+
+buttonDestroyEl.addEventListener("click", () => {
+  divBoxesEl.innerHTML = "";
+  console.log("remove");
+  window.location.reload();
+});
+
+let sizeElement = 30;
+
+function createBoxes(amount) {
+  for (let i = 0; i < amount; i++) {
+    const newDevEl = document.createElement("div");
+    newDevEl.style.width = `${sizeElement}` + "px";
+    newDevEl.style.height = `${sizeElement}` + "px";
+    newDevEl.style.backgroundColor = getRandomHexColor();
+    console.log(newDevEl);
+    divBoxesEl.append(newDevEl);
+    sizeElement += 10;
+  }
+}
